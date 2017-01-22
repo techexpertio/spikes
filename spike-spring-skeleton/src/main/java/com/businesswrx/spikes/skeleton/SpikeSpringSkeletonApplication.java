d9b020d91b71dabb2b5aaf4cbe3fa3b090f33977
Patty Eilers <patricia.eilers@gmail.com>
2017-01-22 09:15:49 -0700

    Initial spring boot project

---
+++ spike-spring-skeleton/src/main/java/com/businesswrx/spikes/skeleton/SpikeSpringSkeletonApplication.java
@@ -0,0 +1,12 @@
+package com.businesswrx.spikes.skeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeSpringSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeSpringSkeletonApplication.class, args);
+	}
+}
